b8614f2981fb78aad893d8fb02940647a4e16d25
T-im4i-k <138623023+T-im4i-k@users.noreply.github.com>
2025-09-12 14:12:00 +0200

    Add Test Code Snippets (#2)
    * Add formatted_code Test
    - Test contain already formatted code
    - Should pass formatting checks
    
    * Add unformatted_code Test
    - Test contain unformatted code
    - Should fail formatting checks

---
+++ test/unformatted_code/test.cpp
@@ -0,0 +1,31 @@
+// Unformatted code, that should fail formatting checks
+
+#include<array>
+   #include    <cassert>
+#include <numeric>
+
+    namespace  Math
+    {
+    auto sqr   ( const   int x)->int     {
+return x       * x;
+}
+    } // namespace Math
+
+    auto   main(    ) ->int{
+    constexpr   std::size_t     ARRAY_SIZE=10;
+    std::array<  int,ARRAY_SIZE  >array={};
+    std::iota(
+            array.begin(),
+            array.end(),
+            1
+);
+
+    for (std::size_t i = 0;
+         i < ARRAY_SIZE; ++i)
+    {
+        assert(
+                Math::sqr(array.at(i))==array.at( i )*array.at( i )
+            );
+    }
+
+    return 0;}
